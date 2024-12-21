0a5db42b2b3307e338cf2c570d998533a7cbe58e
jtakumi <takumi87jn@gmail.com>
2024-12-21 22:15:46 +0900

    data model によってデータを保存

--- BirthDaysData/BirthDaysData/ContentView.swift
+++ BirthDaysData/BirthDaysData/ContentView.swift
@@ -1,14 +1,14 @@
 //___FILEHEADER___
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     //初期データ
-    @State private var friends:[Friends]=[
-        Friends(name: "aaa", birthDay: .now),
-        Friends(name: "bbb", birthDay: Date(timeIntervalSince1970: 100))
-        
-    ]
+    @Query private var friends:[Friends]
+    // Friends の model が context となる
+    @Environment(\.modelContext) private var context
+    
     @State private var newName = ""
     @State private var newBirthdayData = Date.now
     
@@ -27,7 +27,7 @@ struct ContentView: View {
             //ボトムバー
         .safeAreaInset(edge:.bottom){
             VStack(alignment:.center,spacing: 20){
-                Text(" New Birthday Date.").font(.headline)
+                Text(" New Birthday Data").font(.headline)
                 // 未来の日付を選ぶことができない、表示は日にちまで
                 DatePicker(selection:$newBirthdayData,in: Date.distantPast...Date.now,displayedComponents: .date){
                     //名前入力
@@ -38,18 +38,29 @@ struct ContentView: View {
                     let newFriend = Friends(name: newName, birthDay: newBirthdayData)
                     //保存してリストに追加
                     if(newFriend.name != ""){
-                        friends.append(newFriend)
+                        context.insert(newFriend)
                     }
                     // 保存したら入力欄はリセット
                     newName=""
                     newBirthdayData = .now
                 }.bold().foregroundColor(.blue)
                 }
+            .padding()
+            .background(.bar)
             }
+        .task{
+            context.insert(Friends(name: "John", birthDay: .now))
+            // 先に日にちの形式を決定しないと日付が入れられない
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            // エルビス関数を使うことが求められる
+            context.insert(Friends(name:"hiro tanaka",birthDay: dateFormatter.date(from: "2021-01-11") ?? Date(timeIntervalSince1970:0)))
+        }
         }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for:Friends.self,inMemory:true)
 }
