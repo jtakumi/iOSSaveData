196c42b2d413aabbd129247876931fda68dd9ee7
jtakumi <takumi87jn@gmail.com>
2024-12-21 21:03:44 +0900

    finished session 2

---
+++ BirthDaysData/BirthDaysDataTests/BirthDaysDataTests.swift
@@ -0,0 +1,16 @@
+//___FILEHEADER___
+
+import XCTest
+@testable import BirthDaysData
+
+final class BirthDaysDataTests:XCTestCase {
+
+    override func setUpWithError() throws {
+       
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+}
